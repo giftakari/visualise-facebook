ac79a01a0a2fb44a266d602c058e00ca0340412a
Phuong Huynh <huynhpt@gmail.com>
2019-03-05 20:11:47 +1100

    update timestamp_ms and chart type for over 1000

--- src/functions/process-data.js
+++ src/functions/process-data.js
@@ -38,29 +38,28 @@ export const processMessagesData = (data, f) => {
   const labelFormat = formatOptions[f];
 
   const parseLabel = t => parse(t, labelFormat, new Date());
-  const parseTime = t => parse(t, "t", new Date());
+  const parseTime = t => parse(t, "T", new Date());
   const formatTime = t => format(t, labelFormat);
 
   const grouped = _.groupBy(data, d => {
-    return formatTime(parseTime(d.timestamp));
+    return formatTime(parseTime(d.timestamp_ms));
   });
 
   const mapped = _.map(grouped, (value, key) => {
     return { label: key, value: value.length };
   });
 
-  const sorted = _.sortBy(mapped, d => parseLabel(d));
-
+  const sorted = _.sortBy(mapped, d => parseLabel(d.label));
   return sorted;
 };
 
 
 export const processWordData = (data, f) => {
-  
+
   const words = _.map(data, (value, key) => {
     return data[0].content.split(" ")
   });
-  
+
   const flatted = _.flatten(words)
 
   const grouped = _.groupBy(flatted, d => {
